50787a77d7062c47bb264a633cd9a7a0728d4c44
Robin <longbin.lai@gmail.com>
2018-01-07 20:09:37 +0800

    Fix the first version of StaticGraph.

---
+++ src/graph_impl/static_graph/mod.rs
@@ -0,0 +1,4 @@
+
+pub mod graph;
+
+pub use graph_impl::static_graph::graph::{EdgeVec, StaticGraph, DiStaticGraph, UnStaticGraph};
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub mod graph_impl;
 pub mod pattern_matching;
 
 pub use graph_impl::{GraphMap, DiGraphMap, UnGraphMap};
+pub use graph_impl::{EdgeVec, StaticGraph, DiStaticGraph, UnStaticGraph};
 
 #[cfg(test)]
 mod tests {
