33b76077cd1a29857ebbd5d8452ad2f2641ec4fd
Zhengyi Yang <zhengyi.yang@outlook.com>
2018-03-19 15:08:32 +0800

    add reader

--- src/io/reader.rs
+++ src/io/reader.rs
@@ -1,1 +1,104 @@
+use std::marker::PhantomData;
 
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use generic::{Directed, GraphType, Undirected};
+use generic::{GraphTrait, MutGraphTrait};
+use graph_impl::GraphMap;
+use converter::graph::{DiStaticGraphConverter, UnStaticGraphConverter};
+
+pub struct GraphReader<Ty: GraphType> {
+    path_to_nodes: String,
+    path_to_edges: String,
+    separator: String,
+    graph_type: PhantomData<Ty>,
+}
+
+pub type DiGraphReader = GraphReader<Directed>;
+pub type UnGraphReader = GraphReader<Undirected>;
+
+impl<Ty: GraphType> GraphReader<Ty> {
+    pub fn new(path_to_nodes: String, path_to_edges: String) -> Self {
+        GraphReader {
+            path_to_nodes,
+            path_to_edges,
+            separator: ",".to_owned(),
+            graph_type: PhantomData,
+        }
+    }
+
+    pub fn with_separator(path_to_nodes: String, path_to_edges: String, separator: String) -> Self {
+        GraphReader {
+            path_to_nodes,
+            path_to_edges,
+            separator,
+            graph_type: PhantomData,
+        }
+    }
+}
+
+impl<Ty: GraphType> GraphReader<Ty> {
+    pub fn read(&self) -> GraphMap<String, Ty> {
+        let mut g = GraphMap::<String, Ty>::new();
+
+        let file = File::open(&self.path_to_nodes)
+            .expect(&format!("Error when reading {}", &self.path_to_nodes));
+        for line in BufReader::new(file).lines() {
+            let line = line.unwrap();
+            let line_vec: Vec<&str> = line.split(&self.separator).collect();
+            let length = line_vec.len();
+
+            if length < 1 || length > 2 {
+                panic!("Unknown format!")
+            }
+
+            let node_id: usize = line_vec[0].parse().unwrap();
+            let node_label = if length > 1 {
+                Some(line_vec[1].to_owned())
+            } else {
+                None
+            };
+
+            g.add_node(node_id, node_label);
+        }
+
+        let file = File::open(&self.path_to_edges)
+            .expect(&format!("Error when reading {}", &self.path_to_edges));
+        for line in BufReader::new(file).lines() {
+            let line = line.unwrap();
+            let line_vec: Vec<&str> = line.split(&self.separator).collect();
+            let length = line_vec.len();
+
+            if length < 2 || length > 3 {
+                panic!("Unknown format!")
+            }
+
+            let src: usize = line_vec[0].parse().unwrap();
+            let dst: usize = line_vec[1].parse().unwrap();
+            let edge_label = if length > 2 {
+                Some(line_vec[2].to_owned())
+            } else {
+                None
+            };
+
+            g.add_edge(src, dst, edge_label);
+        }
+
+        g
+    }
+}
+
+impl DiGraphReader {
+    pub fn read_to_static(&self) -> DiStaticGraphConverter<String> {
+        let g = self.read();
+        DiStaticGraphConverter::from(g)
+    }
+}
+
+impl UnGraphReader {
+    pub fn read_to_static(&self) -> UnStaticGraphConverter<String> {
+        let g = self.read();
+        UnStaticGraphConverter::from(g)
+    }
+}
--- tests/static_graph.rs
+++ tests/static_graph.rs
@@ -35,10 +35,12 @@ fn test_directed() {
     g.add_node(2, None);
     g.add_node(3, None);
 
-    g.add_edge(0, 1, Some("a"));
     g.add_edge(1, 0, Some("a"));
-    g.add_edge(1, 2, Some("b"));
     g.add_edge(2, 0, Some("a"));
+    g.add_edge(2, 3, Some("b"));
+    g.add_edge(3, 0, Some("a"));
+    g.add_edge(3, 1, Some("a"));
+    g.add_edge(3, 2, Some("a"));
 
     let g = DiStaticGraph::from(g);
 
@@ -46,5 +48,5 @@ fn test_directed() {
 
     let edges: Vec<_> = g.edge_indices().collect();
 
-    assert_eq!(edges, vec![(0, 1), (0, 2), (1, 2), (2, 0)]);
+    assert_eq!(edges, vec![(0, 1), (0, 2), (0, 3), (1, 0), (1, 3), (2, 3)]);
 }
