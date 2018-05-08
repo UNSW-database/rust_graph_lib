aa7b49b2d7a96bce98c7732a7a379f5c35d99303
Zhengyi Yang <zhengyi.yang@outlook.com>
2018-05-08 19:47:04 +0100

    update tests

--- tests/static_graph.rs
+++ tests/static_graph.rs
@@ -10,9 +10,9 @@ fn test_directed() {
     let in_edge_vec = EdgeVec::new(vec![0, 2, 3, 4], vec![1, 2, 0, 0]);
     let g = DiStaticGraph::new(3, edge_vec, Some(in_edge_vec));
 
-    assert_eq!(g.neighbors(0).into_owned(), vec![1, 2]);
-    assert_eq!(g.neighbors(1).into_owned(), vec![0]);
-    assert_eq!(g.neighbors(2).into_owned(), vec![0]);
+    assert_eq!(&g.neighbors(0)[..], &[1, 2]);
+    assert_eq!(&g.neighbors(1)[..], &[0]);
+    assert_eq!(&g.neighbors(2)[..], &[0]);
 
     assert_eq!(g.in_neighbors(0).into_owned(), vec![1, 2]);
     assert_eq!(g.in_neighbors(1).into_owned(), vec![0]);
