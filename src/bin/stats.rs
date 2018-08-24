55a4de6e31a3177ec5cbafb7bba627eceb8a26c1
Zhengyi Yang <zhengyi.yang@outlook.com>
2018-08-24 13:32:34 +0800

    update bins

--- src/bin/stats.rs
+++ src/bin/stats.rs
@@ -21,6 +21,10 @@ fn main() {
         //        let g: UnGraphMap<String> = Deserializer::import(arg).unwrap();
         let g: UnStaticGraph<String> = Deserializer::import(arg).unwrap();
 
+        let max_degree = g.node_indices().map(|i| g.degree(i)).max().unwrap();
+
+        println!("Max degree: {}", max_degree);
+
         let node_labels = g.get_node_label_counter();
         let edge_labels = g.get_edge_label_counter();
 
