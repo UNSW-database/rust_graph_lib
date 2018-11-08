c1e304d62144b3cd048ea2691b43ad18dd51ea73
qingzhu521 <q15000557748@gmail.com>
2018-11-08 19:33:52 +0800

    dfs_stack

--- src/algorithm/dfs.rs
+++ src/algorithm/dfs.rs
@@ -23,10 +23,69 @@ fn dfs<G, NL, EL>(start: DefaultId, graph: Arc<G>) -> Vec<DefaultId>
     where G: GeneralGraph<DefaultId, NL, EL>,
           NL: Eq + Hash,
           EL: Eq + Hash,{
+    let x = graph.node_indices().max().unwrap();
     let mut dfs_order = vec![];
-    let mut visited = vec![false; graph.node_count()];
+    let mut visited = vec![false; x as usize + 1];
 
     dfs_helper(start, &graph, &mut dfs_order, &mut visited);
 
     dfs_order
+}
+
+fn dfs_stack<G, NL, EL>(start: DefaultId, graph: Arc<G>) -> Vec<DefaultId>
+    where G: GeneralGraph<DefaultId, NL, EL>,
+          NL: Eq + Hash,
+          EL: Eq + Hash,{
+    let x = graph.node_indices().max().unwrap();
+    let mut dfs_order = vec![];
+    let mut stack = vec![];
+    let mut visited = vec![false; x as usize + 1];
+
+    dfs_order.push(start);
+    visited[start as usize] = true;
+    stack.push((start, 0));
+    while !stack.is_empty() {
+        let (point, pos) = stack.pop().unwrap();
+        let len = graph.neighbors(point).len();
+        if pos < len{
+            stack.push((point, pos + 1));
+            let next = graph.neighbors(point)[pos];
+            if !visited[next as usize] {
+                dfs_order.push(next);
+                visited[next as usize] = true;
+                stack.push((next,0));
+            }
+        }
+    }
+
+    dfs_order
+}
+
+#[cfg(test)]
+mod test{
+    use super::*;
+    use UnGraphMap;
+    use generic::MutGraphTrait;
+    #[test]
+    fn dfs_test(){
+        let mut graph = UnGraphMap::<u32>::new();
+        graph.add_edge(1,2, None);
+        graph.add_edge(2,3, None);
+        graph.add_edge(3,1, None);
+        graph.add_edge(3,4, None);
+        let graph_arc = Arc::new(graph);
+        let res = dfs(1, graph_arc);
+        assert_eq!(vec![1,2,3,4], res);
+    }
+    #[test]
+    fn dfs_stack_test(){
+        let mut graph = UnGraphMap::<u32>::new();
+        graph.add_edge(1,2, None);
+        graph.add_edge(2,3, None);
+        graph.add_edge(3,1, None);
+        graph.add_edge(3,4, None);
+        let graph_arc = Arc::new(graph);
+        let res = dfs_stack(1, graph_arc);
+        assert_eq!(vec![1,2,3,4], res);
+    }
 }
