2878d9f959d4fd2ce5697a8a5fe09c8dee9632cd
KongzhangHao <haokongzhang@gmail.com>
2018-11-19 17:29:17 +1100

    Removed changes in main

--- src/main.rs
+++ src/main.rs
@@ -22,28 +22,15 @@ extern crate rust_graph;
 
 use rust_graph::graph_impl::UnGraphMap;
 use rust_graph::prelude::*;
-use rust_graph::algorithm;
-
 
 fn main() {
     let g = UnGraphMap::<Void>::new();
 
-
-    algorithm::bfs::test_bfs();
-    algorithm::dfs::test_dfs();
-    algorithm::conn_comp::test_conn_comp();
-
     /// `cargo run` -> The default ID type can hold 4294967295 nodes at maximum.
     /// `cargo run --features=usize_id` -> The default ID type can hold 18446744073709551615 nodes at maximum.
-    algorithm::conn_comp::test_conn_comp();
-    algorithm::bfs::test_bfs();
-    algorithm::dfs::test_dfs();
-
-
     println!(
         "The graph can hold {} nodes and {} labels at maximum.",
         g.max_possible_id(),
         g.max_possible_label_id()
     );
 }
-
