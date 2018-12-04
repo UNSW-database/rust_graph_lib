93720f409ebf264ca7664bfa2d04622722c6f7af
KongzhangHao <haokongzhang@gmail.com>
2018-12-04 11:28:02 +1100

    Changed implementation of ConnSub

--- src/algorithm/conn_subgraphs.rs
+++ src/algorithm/conn_subgraphs.rs
@@ -3,6 +3,7 @@ use generic::dtype::IdType;
 use graph_impl::graph_map::new_general_graphmap;
 use prelude::*;
 use std::hash::Hash;
+use itertools::Itertools;
 
 /// Enumeration of Connected subgraphs of a graph.
 ///
@@ -87,65 +88,31 @@ impl<
     /// Run the graph enumeration by adding each node and edge to the subgraph that it
     /// corresponds to.
     pub fn run_subgraph_enumeration(&mut self, graph: &GeneralGraph<Id, NL, EL, L>) {
-        self.process_nodes(graph);
-        self.process_edges(graph);
-    }
+        if graph.edge_count() != 0 {
+            let mut num_edges:usize = 1;
+            while num_edges <= graph.edge_count() {
+                for edge_vec in graph.edges().combinations(num_edges) {
+                    let mut g_tmp = new_general_graphmap(graph.is_directed());
+                    for edge in edge_vec {
+                        let mut_g = g_tmp.as_mut_graph().unwrap();
+                        let (start, target) = (edge.get_start(), edge.get_target());
 
-    /// Add nodes to their corresponding subgraphs
-    fn process_nodes(&mut self, graph: &GeneralGraph<Id, NL, EL, L>) {
-        for node in graph.nodes() {
-            let id = node.get_id();
-            let root = self.cc.get_root(id).unwrap();
-            let label = graph.get_node_label(id).cloned();
-            let index = self.root_to_subgraph(root);
+                        let node_label_one = graph.get_node_label(start);
+                        let node_label_two = graph.get_node_label(target);
 
-            if let Some(index) = index {
-                self.subgraphs[index]
-                    .as_mut_graph()
-                    .unwrap()
-                    .add_node(id, label);
-            } else {
-                if graph.is_directed() {
-                    self.subgraphs.push(new_general_graphmap(true));
-                } else {
-                    self.subgraphs.push(new_general_graphmap(false));
+                        mut_g.add_node(start, node_label_one.cloned());
+                        mut_g.add_node(target, node_label_two.cloned());
+
+                        let edge_label = graph.get_edge_label(start, target);
+
+                        mut_g.add_edge(start, target, edge_label.cloned());
+                    }
+
+                    if g_tmp.node_count() > 0 && self.cc.get_count() == 1 {
+                        self.subgraphs.push(g_tmp);
+                    }
                 }
-                self.roots.push(root);
-                let length = self.subgraphs.len();
-                self.subgraphs[length - 1]
-                    .as_mut_graph()
-                    .unwrap()
-                    .add_node(id, label);
-            }
-        }
-    }
-
-    /// Add edges to their corresponding subgraphs
-    fn process_edges(&mut self, graph: &GeneralGraph<Id, NL, EL, L>) {
-        for edge in graph.edges() {
-            let start = edge.get_start();
-            let target = edge.get_target();
-            let root = self.cc.get_root(start).unwrap();
-            let label = graph.get_edge_label(start, target).cloned();
-            let index = self.root_to_subgraph(root);
-
-            if let Some(index) = index {
-                self.subgraphs[index]
-                    .as_mut_graph()
-                    .unwrap()
-                    .add_edge(start, target, label);
-            } else {
-                if graph.is_directed() {
-                    self.subgraphs.push(new_general_graphmap(true));
-                } else {
-                    self.subgraphs.push(new_general_graphmap(false));
-                }
-                self.roots.push(root);
-                let length = self.subgraphs.len();
-                self.subgraphs[length - 1]
-                    .as_mut_graph()
-                    .unwrap()
-                    .add_edge(start, target, label);
+                num_edges += 1;
             }
         }
     }
