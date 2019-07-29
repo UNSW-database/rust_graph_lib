8d8e488aab3dbfd3059f28a6be9b3a555903f1c1
Zhengyi Yang <zhengyi.yang@outlook.com>
2019-07-29 13:06:31 +1000

    move hdfs dependency to a separate repo

--- src/io/hdfs/hdfs_lib/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    //Passing environment variable to compiler to prompt the static and shared library path
-    println!("cargo:rustc-link-search=native=src/io/hdfs/hdfs_lib/src/native");
-}
