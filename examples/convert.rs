0413bc69f9b70662de833a10b14b562d5d14a476
Zhengyi Yang <zhengyi.yang@outlook.com>
2020-06-21 17:53:04 +1000

    fmt

--- examples/convert.rs
+++ examples/convert.rs
@@ -1,15 +1,10 @@
-use std::io::*;
-use std::fs::*;
-
-use indexmap::{indexmap};
-use std::path::Path;
-use indexmap::map::IndexMap;
 use indexmap::set::IndexSet;
+use std::path::Path;
 
 // Give a text file of *ID degree* sorted by degree
 // and a text file of edges in the from of *src dst*,
 // reorder the graph by the degree of nodes.
-fn main(){
+fn main() {
     // TODO: Read the first file, put ids in an IndexSet if degree!=0
     let degree_file = std::env::args().nth(1).unwrap();
     let edge_file = std::env::args().nth(2).unwrap();
@@ -33,7 +28,6 @@ fn main(){
     }
 
     // TODO: Read the second file, convert each id using the above mapping and write to a new file
-
     let mut reader = csv::ReaderBuilder::new()
         .has_headers(false)
         .from_path(edge_file)
@@ -41,16 +35,17 @@ fn main(){
 
     let mut wtr = csv::Writer::from_path(output_file).unwrap();
 
-
     for result in reader.records() {
         let record = result.unwrap();
         let src = record[0].trim().parse::<u32>().unwrap();
         let dst = record[1].trim().parse::<u32>().unwrap();
 
-        let edge = vec![map.get_full(&src).unwrap().0.to_string(), map.get_full(&dst).unwrap().0.to_string()];
+        let edge = vec![
+            map.get_full(&src).unwrap().0.to_string(),
+            map.get_full(&dst).unwrap().0.to_string(),
+        ];
         wtr.write_record(&edge).unwrap();
     }
 
     wtr.flush();
-
 }
