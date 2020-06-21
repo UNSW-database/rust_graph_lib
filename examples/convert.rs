93818ea9f21b81caba8ff3cb77b746b0e94e3fe6
Zhengyi Yang <zhengyi.yang@outlook.com>
2020-06-21 17:08:16 +1000

    add convert

---
+++ examples/convert.rs
@@ -0,0 +1,15 @@
+use std::io::*;
+use std::fs::*;
+
+use indexmap::IndexSet;
+
+// Give a text file of *ID degree* sorted by degree
+// and a text file of edges in the from of *src dst*,
+// reorder the graph by the degree of nodes.
+fn main(){
+    // TODO: Read the first file, put ids in an IndexSet if degree!=0
+
+
+    // TODO: Read the second file, convert each id using the above mapping and write to a new file
+
+}
